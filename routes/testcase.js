7ef5c2891d74d5fd448c38177bf30c6e7969df58
Aziz Mahmoudy <aziz.mahmoudy@hotmail.com>
2020-09-11 18:06:53 +0200

    changed the payload format for testcase endpoint

--- routes/testcase.js
+++ routes/testcase.js
@@ -3,67 +3,54 @@ const projectService = require('../services/project');
 
 module.exports = function(ctx) {
     server = ctx.server;
+    const SUCCESS_MSG = {"message": "%s"};
+    const ERROR_MSG = {"message": "could not process the request"};
+
     /*
      * Get testcases list
-     * 
-     * GET http://localhost:3000/testcases
      */
     server.get('/api/testcases', (req, res, next) => {
         testcaseService.getTestcases(null).then(function(result) {
             result
                 ? res.send(200, result)
-                : res.send(400, "testcases for project id '%s' do not exist" + 
-                    req.params.projectId)
+                : res.send(400, ERROR_MSG)
         });               
     })
 
     /*
      * Get testcases list
-     * 
-     * GET http://localhost:3000/projects/AAA01/testcases
      */
     server.get('/api/projects/:projectId/testcases', (req, res, next) => {
         if(req.query.search!=undefined){
-            testcaseService.getTestcases(req.params.projectId, req.query.search).then(function(result) {
+            testcaseService.getTestcases(req.params.projectId, 
+                req.query.search).then(function(result) {
                 result
                     ? res.send(200, result)
-                    : res.send(400, "testcases for project id '" + req.params.projectId + "' do not exist")
+                    : res.send(400, ERROR_MSG)
             }); 
         }else{
             testcaseService.getTestcases(req.params.projectId).then(function(result) {
                 result
                     ? res.send(200, result)
-                    : res.send(400, "testcases for project id '" + req.params.projectId + "' do not exist")
+                    : res.send(400, ERROR_MSG)
             });
         }         
     })
 
     /*
      * Get a testcase
-     * 
-     * GET http://localhost:3000/projects/AAA01/testcases/12
      */
     server.get('/api/projects/:projectId/testcases/:testcaseId', (req, res, next) => {
-       testcaseService.getTestcase(req.params.testcaseId, req.params.projectId).then(function(result) {
+       testcaseService.getTestcase(req.params.testcaseId, 
+        req.params.projectId).then(function(result) {
             result
                 ? res.send(200, result)
-                : res.send(400, "testcase id '" + req.params.testcaseId + "' for project id '" + req.params.projectId + "' does not exist")
+                : res.send(400, ERROR_MSG)
          });     
     })
 
     /**
      * Create
-     * 
-    POST http://localhost:3000/projects/AAA01/testcases 
-     {
-        "id" : 5,
-        "title" : "basic login test 5",
-        "description" : "basic login test for the Web portal application",
-        "steps" : [
-        	{"name" : "login 5", "description" : "submit credential"},
-        	{"name" : "landing page 5", "description" : "user sees his name on the landing page"}
-        	]
-     }
     */
     server.post('/api/projects/:projectId/testcases', (req, res, next) => {
 
@@ -89,39 +76,21 @@ module.exports = function(ctx) {
                 res.send(200, result._doc);
             })
             .catch( err => {
-                console.log("could not create the testcase for project id '%s', error: %s", req.params.projectId, err.errmsg);
-                res.send(400, err.errmsg)
+                console.log("could not create the testcase for project id '%s', error: %s", 
+                req.params.projectId, err.errmsg);
+                res.send(400, ERROR_MSG)
             });
 
          })
         .catch( err => {
-            res.send(400, "the project id '" + req.params.projectId + "' does not exist");
+            res.send(400, ERROR_MSG);
         });
     })
 
     /**
      * Update
-     * 
-        PUT http://localhost:3000/api/projects/AAA01/testcases/4
-        {
-            "id": "4",
-            "title": "basic login test 4",
-            "description": "basic login test for the Web portal application",
-            "steps": [
-                {
-                    "name": "login",
-                    "description": "submit credential"
-                },
-                {
-                    "name": "landing page",
-                    "description": "user sees his name on the landing page"
-                }
-            ],
-            "projectId": "AAA01"
-        }
      */
     server.put('/api/projects/:projectId/testcases/:testcaseId', (req, res, next) => {
-
         // extract data from request body and add timestamps
        const data = Object.assign( {}, req.body, {
             lastUpdated: new Date()
@@ -134,19 +103,19 @@ module.exports = function(ctx) {
             res.send(200, result._doc);
         })
         .catch( err => {
-            res.send(400, {'error_code': 1, 'error_message': 'could not process the request'})
+            res.send(400, ERROR_MSG)
         });        
     })
 
     /**
      * Delete
-     * src: https://docs.mongodb.com/manual/reference/method/db.collection.remove/
      */
     server.del('/api/projects/:projectId/testcases/:testcaseId', (req, res, next) => {
         testcaseService.deleteTestcase(req.params.testcaseId, req.params.projectId).then(function(result) {
              ((result.n) && result.n > 0)
-                 ? res.send(200, {'message': "testcase id '" + req.params.testcaseId + "' for project id '" + req.params.projectId + "' is now deleted"})
-                 : res.send(400, "testcase id '" + req.params.testcaseId + "' for project id '" + req.params.projectId + "' does not exist")
+                 ? res.send(200, {'message': "testcase id '" + req.params.testcaseId + 
+                 "' for project id '" + req.params.projectId + "' is now deleted"})
+                 : res.send(400, ERROR_MSG)
           }); 
     })
 }
