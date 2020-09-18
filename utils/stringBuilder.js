dfa19f51de6accb9d020e86271a047dd07fb6e47
Aziz Mahmoudy <aziz.mahmoudy@hotmail.com>
2020-09-18 13:00:06 +0200

    removed old message builder

--- utils/stringBuilder.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    fillTemplate: function(templateVars){
-        const templateString = '{"message": "${this.content}"}';
-        return new Function("return `"+templateString +"`;").call(templateVars);
-    }
- }
