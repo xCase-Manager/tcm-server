64958e7e194a3d3732501b839a3703b89306cf5f
Aziz Mahmoudy <aziz.mahmoudy@hotmail.com>
2020-10-12 12:34:35 +0200

    added common project search service

--- services/project.js
+++ services/project.js
@@ -4,147 +4,98 @@ const Project = require('../models/project');
         Project service
 *****************************/
 class ProjectService {
-    /*
-        Project find service
-    */
-    findProject(projectId) {
-        return new Promise((resolve, reject) => {
-           console.log("requesting DB for project id: %s", projectId);
+  /*
+  * Project find service
+  */
+  findProject(projectId) {
+    return new Promise((resolve, reject) => {
+      console.log("requesting DB for project id: %s", projectId);
+      Project.findOne(
+        { "id" : projectId}, { '_id': 0, '__v': 0}, 
+        function(dbErr, dbRes) {
+          if (dbRes) resolve(dbRes);
+          else{
+            console.log("not found response: %s", dbErr);
+            reject(dbErr); 
+          }       
+        }
+      );
+    });
+  };
 
-           Project.findOne(
-            { "id" : projectId},
-            { '_id': 0, '__v': 0}, 
+  /*
+  * get a Projects list filtered
+  */
+  async findProjects(filter) {
+    try {
+      return await (new Promise((resolve, reject) => {
+        if(filter!=undefined)
+          Project.find({       
+            $or: [{ "name": {$regex: new RegExp(filter), $options: 'i'}}, 
+                { "description": {$regex: new RegExp(filter), $options: 'i'}}]
+            }, { '_id': 0, '__v': 0 }, 
             function(dbErr, dbRes) {
-                if (dbRes){
-                    console.log("found response: %s", dbRes);
-                    resolve(dbRes);
-                } else{
-                    console.log("not found response: %s", dbErr);
-                    reject(dbErr); 
-                }       
+              if (dbRes) resolve(dbRes);
+              else{
+                console.log("not found response: %s", dbErr);
+                reject(dbErr);}       
+            });
+        else
+          Project.find( {}, { '_id': 0, '__v': 0 }, 
+            function(dbErr, dbRes) {
+              if (dbRes) resolve(dbRes);
+              else{
+                console.log("not found response: %s", dbErr);
+                reject(dbErr); 
+              }         
             }
-            );
-        });
-    };
+        );
+       }))} catch(e) {
+         console.log("error response: %s", e);
+    }    
+  };
 
-    /*
-        get a Projects list
-    */
-    async getProjects() {
-        try {
-            return await (  new Promise((resolve, reject) => {
-                console.log("requesting DB for projects list");
-                Project.find(
-                        { '_id': 0, '__v': 0 }, 
-                        function(dbErr, dbRes) {
-                            if (dbRes){
-                                //console.log("found response: %s", dbRes);
-                                resolve(dbRes);
-                            } else{
-                                console.log("not found response: %s", dbErr);
-                                reject(dbErr); 
-                            }       
-                        }
-                    );
-                })
-            )
-        } catch(e) {
-            console.log("error response: %s", e);
-          }    
-    };
+  /*
+  * save a new Project
+  */
+  async saveProject(data) {
+    try{
+      return await ( new Promise((resolve, reject) => {    
+        Project.create(data, 
+          function(dbErr, dbRes) {
+            if (dbRes){
+              delete dbRes._doc._id;
+              delete dbRes._doc.__v;
+              resolve(dbRes);
+            } else{
+              console.log("not found response: %s", dbErr);
+              reject(dbErr); 
+            }       
+          });
+      }));
+    } catch(e) {
+      console.log("coud not create the project, error:", e);
+    }
+  };
 
-    /*
-        get a Projects list filtered
-    */
-    async getProjects(filter) {
-        try {
-            return await (  new Promise((resolve, reject) => {
-            console.log("requesting DB for projects filtered by '%s'", filter);    
-            Project.find({       
-                            $or: [
-                                { "name": {$regex: new RegExp(filter), $options: 'i'} }, 
-                                { "description": {$regex: new RegExp(filter), $options: 'i'} }
-                            ]
-                        },
-                        { '_id': 0, '__v': 0 }, 
-                        function(dbErr, dbRes) {
-                                if (dbRes){
-                                    //console.log("found response: %s", dbRes);
-                                    resolve(dbRes);
-                                } else{
-                                    console.log("not found response: %s", dbErr);
-                                    reject(dbErr); 
-                                }       
-                        }
-                    );
-                }))
-        } catch(e) {
-                console.log("error response: %s", e);
-            }    
+  /*
+  * update a Project
+  */
+  async update(projectId, data) {
+    try{
+      return await ( new Promise((resolve, reject) => {
+       Project.findOneAndUpdate(
+        { "id" : projectId}, data, { new: true },
+          function(dbErr, dbRes) {
+            if (dbRes) resolve(dbRes);
+            else{
+              console.log("could not update project id '%s', error: %s", 
+              projectId, dbErr);
+              reject(dbErr); 
+            }});
+      }));} catch(e) {
+        console.log("coud not update test id '%s' for project id '%s', error:", 
+        testcaseId, projectId, e);
+      }
     };
-
-    /*
-        save a new Project
-    */
-    async saveProject(data) {
-        try{
-            return await ( new Promise((resolve, reject) => {
-                console.log("requesting DB to create a project");         
-                Project.create(data, 
-                    function(dbErr, dbRes) {
-                        if (dbRes){
-                            console.log("found response: %s", dbRes);
-                            delete dbRes._doc._id;
-                            delete dbRes._doc.__v;
-                            resolve(dbRes);
-                        } else{
-                            console.log("not found response: %s", dbErr);
-                            reject(dbErr); 
-                        }       
-                    }
-                );
-            }));
-        } catch(e) {
-            console.log("coud not create the project, error:", e);
-          }
-    };
-
-    /*
-        update a Project
-    */
-    async update(projectId, data) {
-        try{
-            return await ( new Promise((resolve, reject) => {
-                console.log("requesting DB to update a project for project id '%s'", projectId);
-                console.log("---> data: '%s'", data);
-    
-                Project.findOneAndUpdate(
-                    { "id" : projectId},
-                    data,
-                    { new: true },
-                    function(dbErr, dbRes) {
-                        if (dbRes){
-                            console.log("update response: %s", dbRes);
-                            resolve(dbRes);
-                        } else{
-                            console.log(
-                                    "could not update project id '%s', error: %s", 
-                                    projectId,
-                                    dbErr);
-                            reject(dbErr); 
-                        }       
-                    }
-                );
-            }) 
-                );
-        } catch(e) {
-            console.log(
-                    "coud not update test id '%s' for project id '%s', error:", 
-                    testcaseId, 
-                    projectId,
-                    e
-                );
-            }
-    };
-}
-module.exports = new ProjectService()
+} module.exports = new ProjectService()
