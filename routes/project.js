ba9da06b8cfcbaa1373a336cb6a4ce0bc286a164
Aziz Mahmoudy <aziz.mahmoudy@hotmail.com>
2020-10-12 12:48:29 +0200

    set updated services in controllers

--- routes/project.js
+++ routes/project.js
@@ -1,4 +1,3 @@
-const Project = require('../models/project');
 const projectService = require('../services/project');
 const messageBuilder = require('../utils/messageBuilder');
 
@@ -6,105 +5,73 @@ const messageBuilder = require('../utils/messageBuilder');
         Project route controller
 *****************************/
 module.exports = function(ctx) {
-    const db = ctx.db,
-    server = ctx.server;
-    const ERROR_MSG = "could not process the request";
+  const db = ctx.db,
+  server = ctx.server;
+  const ERROR_MSG = "could not process the request";
   
-   server.post('/api/projects', (req, res, next) => {
+  server.post('/api/projects', (req, res, next) => {
+    const data = Object.assign({}, req.body, 
+        { created: new Date()}
+    );
+    projectService.saveProject(data)
+      .then( result => res.send(200, result._doc))
+      .catch( err => {
+        console.log("could not create the project, error: %s", err.errmsg);
+        res.send(400, messageBuilder.error(
+          "could not create project, this project id might already exist"))
+      });
+  })
 
-        const data = Object.assign( 
-            {}, 
-            req.body, 
-            {
-                created: new Date()
-            }
-        );
+  /**
+  * Get a project
+  */
+  server.get('/api/projects/:projectId', (req, res, next) => {
+    projectService.findProject(req.params.projectId).then(function(result) {
+      result? res.send(200, result)
+      :res.send(400, messageBuilder.error(ERROR_MSG))
+    }).catch( err => res.send(400, messageBuilder.error(ERROR_MSG))); 
+  })
 
-        projectService.saveProject(data)
-        .then( result => {        
-            res.send(200, result._doc);
-        })
-        .catch( err => {
-            console.log("could not create the project, error: %s", err.errmsg);
-            res.send(400, messageBuilder.error(
-                "could not create project, this project id might already exist"))
-        });
+  /**
+  * Get all projects
+  */
+  server.get('/api/projects', (req, res, next) => {
+    projectService.findProjects(req.query.search).then(function(result) {
+      result? res.send(200, result)
+      :res.send(400, messageBuilder.error(ERROR_MSG))
+    }); 
+  })
 
+  /**
+  * Delete ProjectId
+  */
+  server.del('/api/projects/:projectId', (req, res, next) => {
+    db.collection("projects").remove(
+      { "id" : req.params.projectId}, 
+      { "justOne": true },
+      function(dbErr, dbRes) {
+        if (dbErr) res.send(400, dbErr.errmsg)
+        if (dbRes){
+          if(dbRes.result.n==0)
+            res.send(400, messageBuilder.error(ERROR_MSG))                  
+          else
+            res.send(200, SUCCESS_MSG,
+              "project id '" + req.params.projectId + 
+              "' has been succefully deleted")
+        }           
     })
+   })
 
-    /**
-     * Get a project
-     */
-    server.get('/api/projects/:projectId', (req, res, next) => {
+  /**
+  * Update project
+  */
+  server.put('/api/projects/:projectId', (req, res, next) => {
+    const data = Object.assign( {}, req.body, {
+      lastUpdated: new Date()
+    });
 
-        projectService.findProject(req.params.projectId).then(function(result) {
-            result
-                ? res.send(200, result)
-                : res.send(400, messageBuilder.error(ERROR_MSG))
-        }).catch(
-            err => {
-                res.send(400, messageBuilder.error(ERROR_MSG))
-            }
-        ); 
-    })
-
-    /**
-     * Get all projects
-     */
-    server.get('/api/projects', (req, res, next) => {
-        
-        if(req.query.search!=undefined){
-            projectService.getProjects(req.query.search).then(function(result) {
-                result
-                    ? res.send(200, result)
-                    : res.send(400, messageBuilder.error(ERROR_MSG))
-            }); 
-        }else{
-            projectService.getProjects().then(function(result) {
-                result
-                    ? res.send(200, result)
-                    : res.send(400, messageBuilder.error(ERROR_MSG))
-            });
-        }
-    })
-
-    /**
-     * Delete ProjectId
-     */
-    server.del('/api/projects/:projectId', (req, res, next) => {
-
-        db.collection("projects").remove(
-            { "id" : req.params.projectId}, 
-            { "justOne": true },
-            function(dbErr, dbRes) {
-                if (dbErr)
-                    res.send(400, dbErr.errmsg)
-                if (dbRes){
-                    if(dbRes.result.n==0)
-                        res.send(400, messageBuilder.error(ERROR_MSG))                  
-                    else
-                        res.send(200, SUCCESS_MSG,
-                            "project id '" + req.params.projectId + 
-                            "' has been succefully deleted")
-                }           
-            }
-         )
-    })
-
-    /**
-     Update project
-     */
-    server.put('/api/projects/:projectId', (req, res, next) => {
-        const data = Object.assign( {}, req.body, {
-            lastUpdated: new Date()
-        });
-
-        projectService.update(req.params.projectId, data)
-        .then( result => {        
-            res.send(200, result._doc);
-        })
-        .catch( err => {
-            res.send(400, messageBuilder.error(ERROR_MSG))
-        });            
-    })
+    projectService.update(req.params.projectId, data)
+      .then(result => res.send(200, result._doc))
+      .catch(err => res.send(400, messageBuilder.error(ERROR_MSG)));            
+  })
 }
--- routes/testcase.js
+++ routes/testcase.js
@@ -6,110 +6,99 @@ const messageBuilder = require('../utils/messageBuilder');
         Testcase route controller
 *****************************/
 module.exports = function(ctx) {
-    server = ctx.server;
-    const ERROR_MSG = "could not process the request";
+  server = ctx.server;
+  const ERROR_MSG = "could not process the request";
 
-    /*
-     * Get all testcases list
-     */
-    server.get('/api/testcases', (req, res) => {
-        testcaseService.getTestcases(null).then(function(result) {
-            result? res.send(200, result)
-            : res.send(400, messageBuilder.error(ERROR_MSG))
-        });
-    })
+  /*
+  * Get all testcases list
+  */
+  server.get('/api/testcases', (req, res) => {
+    testcaseService.getTestcases(undefined, req.query.search).then(function(result) {
+      result? res.send(200, result)
+      :res.send(400, messageBuilder.error(ERROR_MSG))
+    });
+  })
 
-    /*
-     * Get a project's testcases list
-     */
-    server.get('/api/projects/:projectId/testcases', (req, res) => {
-        if(req.query.search!=undefined){
-            testcaseService.getTestcases(req.params.projectId, req.query.search)
-            .then(function(result) {
-                result? res.send(200, result)
-                : res.send(400, messageBuilder.error(ERROR_MSG))
-            }); 
-        } else{
-            testcaseService.getTestcases().then(function(result) {
-                result? res.send(200, result)
-                : res.send(400, messageBuilder.error(ERROR_MSG))
-            });
-        }
-    })
+  /*
+  * Get a project's testcases list 
+  */
+  server.get('/api/projects/:projectId/testcases', (req, res) => {
+    testcaseService.getTestcases(
+     req.params.projectId, req.query.search)
+     .then(function(result) {
+       result? res.send(200, result)
+       :res.send(400, messageBuilder.error(ERROR_MSG))
+     });
+  })
 
-    /*
-     * Get a project's testcase
-     */
-    server.get('/api/projects/:projectId/testcases/:testcaseId', (req, res) => {
-        testcaseService.getTestcase(req.params.testcaseId, 
-        req.params.projectId).then(function(result) {
-            result
-                ? res.send(200, result)
-                : res.send(400, messageBuilder.error(ERROR_MSG))
-         });     
-    })
+  /*
+  * Get a project's testcase
+  */
+  server.get('/api/projects/:projectId/testcases/:testcaseId', 
+  (req, res) => {
+    testcaseService.getTestcase(req.params.testcaseId, 
+    req.params.projectId).then(function(result) {
+      result? res.send(200, result)
+      :res.send(400, messageBuilder.error(ERROR_MSG))
+    });     
+  })
 
-    /**
-     * Create a project's testcase
-    */
-    server.post('/api/projects/:projectId/testcases', (req, res) => {
-        projectService.getProject(req.params.projectId)
-        .then(function() {
-            const data = Object.assign( 
-                {}, 
-                req.body, 
-                {
-                    projectId: req.params.projectId,
-                    created: new Date()
-                }
-            );
+  /*
+  * Create a project's testcase
+  */
+  server.post('/api/projects/:projectId/testcases', (req, res) => {
+  projectService.getProject(req.params.projectId)
+    .then(function() {
+      const data = Object.assign( {}, req.body, 
+      {
+        projectId: req.params.projectId,
+        created: new Date()
+      }
+    );
 
-            testcaseService.saveTestcase(data)
-            .then( result => {        
-                res.send(200, result._doc);
-            })
-            .catch( err => {
-                console.log("could not create the testcase for project id '%s', error: %s", 
-                req.params.projectId, err.errmsg);
-                res.send(400, messageBuilder.error(ERROR_MSG))
-            });
-         })
-        .catch( err => {
-            res.send(400, messageBuilder.error(ERROR_MSG));
-        });
-    })
+    testcaseService.saveTestcase(data)
+    .then( result => { res.send(200, result._doc);})
+    .catch( err => {
+      console.log("could not create the testcase for project id '%s', " +
+      "error: %s", 
+      req.params.projectId, err.errmsg);
+      res.send(400, messageBuilder.error(ERROR_MSG))
+    });
+  }).catch( err => {
+    res.send(400, messageBuilder.error(ERROR_MSG));
+  });})
 
-    /**
-     * Update a project's testcase
-     */
-    server.put('/api/projects/:projectId/testcases/:testcaseId', (req, res) => {
-       const data = Object.assign( {}, req.body, {
-            lastUpdated: new Date()
-        });
+  /**
+  * Update a project's testcase
+  */
+  server.put('/api/projects/:projectId/testcases/:testcaseId', (req, res) => {
+    const data = Object.assign( {}, req.body, {
+      lastUpdated: new Date()
+    });
 
-        testcaseService.updateTestcase(req.params.testcaseId, req.params.projectId, data)
-        .then( result => {        
-            res.send(200, result._doc);
-        })
-        .catch( err => {
-            res.send(400, messageBuilder.error(
-                "testcase id '" + req.params.testcaseId + 
-                "' for project id '" + req.params.projectId + 
-                "' could not be updated"))
-        });        
-    })
+    testcaseService.updateTestcase(req.params.testcaseId, 
+      req.params.projectId, data)
+      .then( result => {        
+        res.send(200, result._doc);
+      }).catch( err => {
+        res.send(400, messageBuilder.error(
+          "testcase id '" + req.params.testcaseId + 
+          "' for project id '" + req.params.projectId + 
+          "' could not be updated"))
+      });        
+  })
 
-    /**
-     * Delete a project's testcase
-     */
-    server.del('/api/projects/:projectId/testcases/:testcaseId', (req, res) => {
-        testcaseService.deleteTestcase(req.params.testcaseId, req.params.projectId).then(function(result) {
-             ((result.n) && result.n > 0)
-                 ? res.send(200)               
-                : res.send(400, messageBuilder.error(
-                    "testcase id '" + req.params.testcaseId + 
-                    "' for project id '" + req.params.projectId + 
-                    "' could not be deleted"))
-          }); 
-    })
+  /**
+  * Delete a project's testcase
+  */
+  server.del('/api/projects/:projectId/testcases/:testcaseId', (req, res) => {
+    testcaseService.deleteTestcase(
+      req.params.testcaseId, req.params.projectId).then(function(result) {
+        ((result.n) && result.n > 0)
+        ?res.send(200) :res.send(400, messageBuilder.error(
+        "testcase id '" + req.params.testcaseId + 
+        "' for project id '" + req.params.projectId + 
+        "' could not be deleted"))
+      }); 
+  })
 }
