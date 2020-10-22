9ab115ac51a6290a66ef1abf7cab7af4ca2126fa
Aziz Mahmoudy <aziz.mahmoudy@hotmail.com>
2020-10-22 18:40:18 +0200

    added project POST api test

--- test-scripts/test.js
+++ test-scripts/test.js
@@ -14,9 +14,24 @@ describe('GET api/projects/:id', function() {
       .get('api/projects/AAA01') 
       .set('Accept', 'application/json') 
       .expect(200, { 
-        id: 'AAA'
+        id: 'AAA01'
       }, done) 
   }) 
+})
+
+describe('POST api/projects', function() { 
+  it('returns the project', function() { 
+    return request(app) 
+      .post('api/projects/AAA01') 
+      .set('Content-type', 'application/json') 
+      .expect(201, {
+          "id": "DDD01",
+          "name": "project DDD01",
+          "description": "test desc DDD02",
+          "status": 0,
+          "icon": "assets/projects/AI.svg"
+        }, done) 
+    }) 
 })
 
 describe('GET api/projects/:project-id/testcases', function() { 
